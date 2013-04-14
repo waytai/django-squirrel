157ac717a66fccb9bb8547c3284d7e3e01370a4d
Helmut Irle <helmut@devigninnovation.com>
2013-04-14 15:20:12 +0200

    Import local settings.

--- django_squirrel/settings.py
+++ django_squirrel/settings.py
@@ -120,8 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
@@ -154,3 +153,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
