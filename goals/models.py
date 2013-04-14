0b951c60f75a024f29bba4c94cb64291a838fabc
Helmut Irle <helmut@devigninnovation.com>
2013-04-14 15:51:15 +0200

    Added model for goals app.

--- goals/models.py
+++ goals/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Goal(models.Model):
+
+    STATUS_CHOICES = (
+        (0, 'Inactive'),
+        (1, 'Achieved'),
+        (2, 'Ongoing'),
+    )
+
+    created_date = models.DateTimeField()
+    created_by = models.ForeignKey(User, related_name="+")
+    user = models.ForeignKey(User, related_name="goals")
+    end_amount = models.IntegerField(null=True)
+    current_amount = models.IntegerField()
+    target_date = models.DateField(null=True)
+    status = models.SmallIntegerField(choices=STATUS_CHOICES)
