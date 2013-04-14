43dab8a73509e9b4b7fcae6343c08c4120ec70cd
Helmut Irle <helmut@devigninnovation.com>
2013-04-14 16:24:41 +0200

    Goal model changes.

--- goals/models.py
+++ goals/models.py
@@ -5,15 +5,17 @@ from django.contrib.auth.models import User
 class Goal(models.Model):
 
     STATUS_CHOICES = (
-        (0, 'Inactive'),
-        (1, 'Achieved'),
-        (2, 'Ongoing'),
+        (0, 'Inactive'),  # savings entered manually
+        (1, 'Achieved'),  # target reached
+        (2, 'Ongoing'),  # specific amount saved over recurring period
     )
 
-    created_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name="+")
+    modified_date = models.DateTimeField(auto_now=True)
+    modified_by = models.ForeignKey(User, related_name="+")
     user = models.ForeignKey(User, related_name="goals")
-    end_amount = models.IntegerField(null=True)
+    end_amount = models.IntegerField(null=True, blank=True)
     current_amount = models.IntegerField()
-    target_date = models.DateField(null=True)
+    target_date = models.DateField(null=True, blank=True)
     status = models.SmallIntegerField(choices=STATUS_CHOICES)
