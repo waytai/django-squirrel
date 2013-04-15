a412c9b4a9e701aadc660b17d0a2e6469809f3fe
Helmut Irle <helmut@devigninnovation.com>
2013-04-15 19:50:05 +0200

    Added Transactions as an inline for goals on the admin interface.

--- goals/admin.py
+++ goals/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from goals.models import Goal
+from goals.models import Goal, Transaction
+
+
+class TransactionInline(admin.TabularInline):
+    model = Transaction
+    extra = 1
 
 
 class GoalAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        TransactionInline
+    ]
+
+
 admin.site.register(Goal, GoalAdmin)
