64c593d52c89f5ea84fad3ebad26534a276cc9d4
Helmut Irle <helmut@devigninnovation.com>
2013-04-16 17:40:28 +0200

    Added render function to include request context instance.

--- goals/views.py
+++ goals/views.py
@@ -1,3 +1,12 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def render_response(req, *args, **kwargs):
+    kwargs['context_instance'] = RequestContext(req)
+    return render_to_response(*args, **kwargs)
+
+
 def dashboard(request):
     pass
 
