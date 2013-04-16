f0c004be049e1964e4efed1bd0440b6063f2c4af
Helmut Irle <helmut@devigninnovation.com>
2013-04-16 17:26:22 +0200

    Added static files settings to settings.py.

--- django_squirrel/settings.py
+++ django_squirrel/settings.py
@@ -1,4 +1,7 @@
-# Django settings for django_squirrel project.
+import os
+
+PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
+BASE_PATH = os.path.dirname(PROJECT_PATH)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -61,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,6 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
