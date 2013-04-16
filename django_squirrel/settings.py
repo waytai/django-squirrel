e18938b0c566f7f9c20e1c15e29a14c4ee828c15
Helmut Irle <helmut@devigninnovation.com>
2013-04-16 17:44:18 +0200

    Added template path settings.

--- django_squirrel/settings.py
+++ django_squirrel/settings.py
@@ -115,6 +115,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_PATH, 'templates'),
+    os.path.join(BASE_PATH, 'goals/templates/goals'),
 )
 
 INSTALLED_APPS = (
